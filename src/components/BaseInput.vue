20f3e510d7afa5936b0aa66a9596b1277f1fe14a
Yuan <yuanliu2065@gmail.com>
2022-01-14 20:30:48 +0100

    Wip · Baseinput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    {{ inputValue }}
+    <input :type="type" v-model="inputValue" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    value: {
+      type: [String, Number],
+      default: null,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+  data() {
+    return {
+      inputValue: null,
+    };
+  },
+
+  computed: {
+    compValue() {
+      switch (this.type) {
+        case "number":
+      }
+    },
+  },
+
+  methods: {
+    handleInput(e) {
+      console.log(e.target.value);
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <base-input type="number" v-model="input" />
+  </div>
+</template>
+
+<script>
+import BaseInput from "../components/BaseInput.vue";
+export default {
+  components: { BaseInput },
+  data() {
+    return {
+      input: null,
+    };
+  },
+  methods: {
+    handleInput(e) {
+      console.log(e.target.value);
+    },
+  },
+};
+</script>
